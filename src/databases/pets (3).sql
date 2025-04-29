a03e690e03408aee96360335652a8905570eaca2
jemss31 <jamessvenansali@gmail.com>
2025-04-30 01:26:41 +0800

    New Transactions in users Time date - Home

--- src/databases/pets (3).sql
+++ src/databases/pets (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2025 at 10:01 PM
+-- Generation Time: Apr 29, 2025 at 07:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,11 +28,56 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `appointments` (
-  `id` int(11) NOT NULL,
-  `date` varchar(255) NOT NULL,
-  `time` varchar(255) NOT NULL,
-  `cost` int(11) NOT NULL
+  `a_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `pet_name` varchar(255) NOT NULL,
+  `haircut_id` int(11) NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`a_id`, `date`, `time`, `pet_name`, `haircut_id`, `cost`, `user_id`) VALUES
+(1, '2025-04-30', '10:00:00', 'Pet Name', 2, 30.00, 1),
+(2, '2025-04-30', '10:00:00', 'Jaypee', 3, 20.00, NULL),
+(3, '2025-04-30', '10:00:00', 'Nutella', 1, 25.00, NULL),
+(4, '2025-05-01', '10:00:00', 'kabayo', 2, 30.00, NULL),
+(5, '2025-05-02', '10:00:00', 'Pet Name', 6, 22.00, NULL),
+(6, '2025-05-03', '10:00:00', 'Doggo', 1, 25.00, 1),
+(7, '2025-05-14', '10:00:00', 'nut', 7, 27.00, 26),
+(8, '2025-05-06', '10:00:00', 'Andre', 3, 20.00, 33),
+(9, '2025-05-07', '10:00:00', 'Itlog', 4, 28.00, 26),
+(10, '2025-05-07', '15:30:00', 'Sabio', 6, 22.00, 26);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `haircuts`
+--
+
+CREATE TABLE `haircuts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `cost` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `haircuts`
+--
+
+INSERT INTO `haircuts` (`id`, `name`, `cost`) VALUES
+(1, 'Edgar Cut', 25.00),
+(2, 'Mohowk', 30.00),
+(3, 'Mushroom', 20.00),
+(4, 'Croptop', 28.00),
+(5, 'Lion Cut', 35.00),
+(6, 'Top Knot', 22.00),
+(7, 'Flat Top', 27.00),
+(8, 'Teddy Bear Cut', 24.00);
 
 -- --------------------------------------------------------
 
@@ -46,6 +91,17 @@ CREATE TABLE `logs` (
   `log_action` varchar(255) NOT NULL,
   `log_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`log_id`, `id`, `log_action`, `log_date`) VALUES
+(1, 1, 'Admin updated user info: jemss31 (User ID: 1)', '2025-04-29 13:58:20'),
+(2, 1, 'Admin updated user info: jemss31 (User ID: 1)', '2025-04-29 14:05:33'),
+(3, 1, 'Admin updated user info: jemss31 (User ID: 1)', '2025-04-29 14:13:06'),
+(4, 1, 'Admin updated user info: jemss31 (User ID: 1)', '2025-04-29 14:57:05'),
+(5, 1, 'Admin updated user info: jemss31 (User ID: 1)', '2025-04-29 14:57:18');
 
 -- --------------------------------------------------------
 
@@ -74,12 +130,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `u_fname`, `u_lname`, `u_email`, `type`, `cont`, `u_username`, `u_pass`, `status`, `SecurityQuestion`, `answer`, `image`, `PIN`) VALUES
-(1, 'james sven', 'ansali', 'jamessvenansali@gmail.com', 'Admin', '999999999999', 'jemss31', 'bbd79e22702a1e046b0e59a292b81973d4380edcdfb5dba2322cc86f59c1b5be', 'Aprroved', 'how many finger?', '5', 'src/profiles/hanna.jpg', '555555'),
-(26, 'kevin', 'Kevinn', 'kevin@gmail.com', 'Customer', '8764323456', 'kevin1', '12380d8e99984534a7031f6f3d2aa63032de3ddd450053306011ba8a0f1a261d', 'Aprroved', '', '', '', NULL),
+(1, 'james sven', 'ansali', 'jamessvenansali@gmail.com', 'Admin', '999999999999', 'jemss31', 'bbd79e22702a1e046b0e59a292b81973d4380edcdfb5dba2322cc86f59c1b5be', 'Aprroved', 'how many finger?', '5', 'src/Profiles/23.png', '555555'),
+(26, 'kevin', 'Kevinn', 'kevin@gmail.com', 'Customer', '8764323456', 'kevin1', 'ec04613f62cf13619815224e8e418a78e032ca2e26db6e520b17aef72774d5da', 'Aprroved', '', '', '', '123456'),
 (29, 'gwapo', 'gwapooo', 'kevinn@gmail.com', 'Customer', '0', 'kevin', 'ec04613f62cf13619815224e8e418a78e032ca2e26db6e520b17aef72774d5da', 'Pending', '', '', '', NULL),
 (31, 'double', 'mint', 'mint@gmail.com', 'Customer', '09392265335', 'kevin2', 'ec04613f62cf13619815224e8e418a78e032ca2e26db6e520b17aef72774d5da', 'Aprroved', 'are you gay?', 'no no no', '', '555555'),
 (32, 'khyl', 'Khyl', 'khyl@gmail.com', 'Customer', '09392265335', 'khyl2', '38df64d81b9cafcef76c3528799b2ad791cc6e12b304561472f4af28b836f3ec', 'Pending', '', '', '', NULL),
-(33, 'charles', 'mala', 'mala@gmail.com', 'Customer', '09392265335', 'charles', '151088d7d32987ac3a10e1d462acd20c02a11faa83ec89e45f61ad9b091cd323', 'Pending', '', '', '', NULL),
+(33, 'charles', 'mala', 'mala@gmail.com', 'Customer', '09392265335', 'charles', '8ceb97b3e7a3ed6e6afbe95978431f8ff13d1c8b695a1fe7a3ccb41d9c8b19a4', 'Approved', '', '', '', '111111'),
 (42, 'ansali', 'ansali', 'ansali@gmail.com', 'Admin', '095263426343', 'ansali1', '8ceb97b3e7a3ed6e6afbe95978431f8ff13d1c8b695a1fe7a3ccb41d9c8b19a4', 'Pending', 'how many shits?', 'gwapo', '', NULL),
 (43, 'rasil', 'ansali', 'razhielansali@gmail.com', 'Customer', '093922653151', 'rasil', '5ae4b6d4af4e10776281bb948f6b66b0babc41bc73f96f70abc93d807f5095a9', 'Approved', 'humot ako ilok jeven?', 'humot kaayo', '', NULL),
 (44, 'khyl', 'ansali', 'khylcanonigoansali@gmail.com', 'Admin', '092352524523', 'khyl123', '38df64d81b9cafcef76c3528799b2ad791cc6e12b304561472f4af28b836f3ec', 'Approved', 'shesh?', 'no', '', NULL),
@@ -93,7 +149,16 @@ INSERT INTO `users` (`id`, `u_fname`, `u_lname`, `u_email`, `type`, `cont`, `u_u
 -- Indexes for table `appointments`
 --
 ALTER TABLE `appointments`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`a_id`),
+  ADD KEY `haircut_id` (`haircut_id`),
+  ADD KEY `fk_user` (`user_id`);
+
+--
+-- Indexes for table `haircuts`
+--
+ALTER TABLE `haircuts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `logs`
@@ -117,13 +182,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `haircuts`
+--
+ALTER TABLE `haircuts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -134,6 +205,12 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `logs`
